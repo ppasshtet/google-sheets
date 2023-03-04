4a04f3bb2b4975845171e0c719d19188ebe89503
ppasshtet <quint905@gmail.com>
2023-03-04 20:36:00 +0100

    small traning with analog google sheets by using websocket

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const ws = require("ws")
+const fs = require("fs")
+const http = require("http")
+
+const index = fs.readFileSync("public/index.html", "utf8")
+
+const HOST = "127.0.0.1"
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end(index)
+})
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+})
+
+const wss = new ws.WebSocketServer({server})
+const messages = []
+
+wss.on("connection", (websocketConnection, req) => {
+    const ip = req.socket.remoteAddress
+    console.log(`[open] Connected ${ip}`);
+
+    broadcastMessages(messages, websocketConnection)
+
+    websocketConnection.on("message", (message) => {
+        console.log("[message] Received: " + message);
+        messages.push(message)
+
+        broadcastMessage(message, websocketConnection)
+    })
+
+    websocketConnection.on("close", () => {
+        console.log(`[close] Disconnected ${ip}`);
+    })
+})
+
+function broadcastMessages(message, client) {
+    message.forEach((message) => {
+        if(client.readyState === ws.OPEN) {
+            client.send(message, {binnary: false})
+        }
+    })
+}
+
+function broadcastMessage(message, websocketConnection) {
+    wss.clients.forEach((client) => {
+        if(client.readyState === ws.OPEN && client !== websocketConnection) {
+            client.send(message, {binary: false})
+        }
+    })
+}
